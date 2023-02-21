e9366e0917071acf6639b90f66d85994da2b5a6d
mostafa-kamal <mmkamal021@gmail.com>
2023-02-22 00:58:41 +0600

    commited

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,12 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error('testing async errors')
-  // res.status(200).json({ msg: 'products testing routes' })
+  // throw new Error('testing async errors')
+  const products = await Product.find({
+    // featured: true,
+    name: 'vase table',
+  })
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
--- populate.js
+++ populate.js
@@ -8,9 +8,13 @@ const jsonProducts = require('./products.json')
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
+    await Product.deleteMany()
+    await Product.create(jsonProducts)
     console.log('SUCCESS!!!!')
+    process.exit(0)
   } catch (error) {
     console.log(error)
+    process.exit(1)
   }
 }
 
