944cd0c05805cf72eb868a85a6f23b2fa8a6693c
mostafa-kamal <mmkamal021@gmail.com>
2023-02-22 19:02:23 +0600

    commited

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/api/v1/products', productsRouter)
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
 
-// const port = prcess.env.PORT || 3000
+// const port = process.env.PORT || 3000
 const port = 3000
 
 const start = async () => {
--- controllers/products.js
+++ controllers/products.js
@@ -2,15 +2,84 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
   // throw new Error('testing async errors')
+  // const search = 'ab'
+  /*
   const products = await Product.find({
     // featured: true,
-    name: 'vase table',
-  })
+    // name: 'vase table',
+    // page: '2',
+    // name: { $regex: search, $options: 'i' },
+  }).sort('-name price')
+  */
+  const products = await Product.find({ price: { $gt: 30 } })
+    // .sort('name')
+    .sort('price')
+    .select('name price')
+  // .limit(4)
+  // .skip(5)
   res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' })
+  // console.log(req.query)
+  const { featured, company, name, sort, fields, numericFilters } = req.query
+  const queryObject = {}
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
+  if (company) {
+    queryObject.company = company
+  }
+  if (name) {
+    // queryObject.name = name
+    queryObject.name = { $regex: name, $options: 'i' }
+  }
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    }
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    )
+    console.log(filters)
+  }
+
+  console.log(queryObject)
+  let result = Product.find(queryObject)
+
+  // sort
+
+  if (sort) {
+    // products = products.sort()
+    // console.log(sort)
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createAt')
+  }
+
+  // fields
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
+  }
+
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
+  const products = await result
+  // const products = await Product.find(queryObject)
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {
